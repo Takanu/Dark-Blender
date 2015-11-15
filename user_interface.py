cd0a0a62f2e82d4093a2cba8ebd1223a70474f92
Takanu <crocadillian@me.com>
2015-11-15 11:48:38 +0000

    UI Tweaks
    Adjusted the UI so the Mesh Extract tool appears only in mesh edit
    mode.  A few other things I guess?

--- user_interface.py
+++ user_interface.py
@@ -4,11 +4,11 @@ from bpy.types import Menu, Panel, AddonPreferences, PropertyGroup, UIList
 from rna_prop_ui import PropertyPanel
 
 #Generates the UI panel inside the 3D view
-class GX_Export(Panel):
+class DB_Object(Panel):
     bl_space_type = "VIEW_3D"
     bl_region_type = "TOOLS"
     bl_context = "objectmode"
-    bl_label = "Dark Blender"
+    bl_label = "Object Tools"
     bl_category = "Dark Blender"
 
     def draw(self, context):
@@ -23,15 +23,37 @@ class GX_Export(Panel):
         col_export.separator()
 
         col_export.label("Sculpt/Organic Mesh Tools")
-        col_export.operator("scene.db_autoseam")
         col_export.operator("scene.db_prepdynatopo")
-        col_export.operator("scene.gx_extractmesh")
+        #col_export.operator("scene.gx_extractmesh")
+        col_export.separator()
+
+        col_export.label("Topology Tools")
         col_export.operator("scene.db_generatehp")
         col_export.operator("scene.db_generatelp")
         col_export.operator("scene.db_quickdecimate")
+        col_export.operator("scene.db_quickretopo")
         col_export.separator()
 
-        col_export.label("Additional Primitives")
+        col_export.label("Utilities")
+        col_export.operator("scene.db_autoseam")
         col_export.operator("scene.db_resym_x")
-        col_export.operator("scene.db_quickretopo")
         col_export.operator("scene.db_unfoldhalf")
+
+
+class DB_Edit(Panel):
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "TOOLS"
+    bl_context = "mesh_edit"
+    bl_label = "Object Tools"
+    bl_category = "Dark Blender"
+
+    def draw(self, context):
+        layout = self.layout
+
+        scn = context.scene.GXScn
+        ob = context.object
+
+        col_export = layout.column(align=True)
+        col_export.label("Utilities")
+        col_export.operator("scene.gx_extractmesh")
+        col_export.separator()
